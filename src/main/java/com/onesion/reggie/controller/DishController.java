3ddbc33bf19bf461ac5c5d55c7a279501454295b
onesion <onesioner@163.com>
2025-03-16 15:33:43 +0800

    菜品展示-查询菜品方法修改

--- src/main/java/com/onesion/reggie/controller/DishController.java
+++ src/main/java/com/onesion/reggie/controller/DishController.java
@@ -6,6 +6,7 @@ import com.onesion.reggie.common.R;
 import com.onesion.reggie.dto.DishDto;
 import com.onesion.reggie.entity.Category;
 import com.onesion.reggie.entity.Dish;
+import com.onesion.reggie.entity.DishFlavor;
 import com.onesion.reggie.service.CategoryService;
 import com.onesion.reggie.service.DishFlavorService;
 import com.onesion.reggie.service.DishService;
@@ -40,23 +41,23 @@ public class DishController {
      * @param dish
      * @return
      */
-    @GetMapping("/list")
-    public R<List<Dish>> list(Dish dish) {
-
-        //构造查询条件
-        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(dish.getCategoryId() != null, Dish::getCategoryId, dish.getCategoryId());
-
-        //添加条件，查询状态为1（起售状态）的菜品
-        queryWrapper.eq(Dish::getStatus, 1);
-
-        //添加排序条件
-        queryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
-
-        List<Dish> list = dishService.list(queryWrapper);
-
-        return R.success(list);
-    }
+//    @GetMapping("/list")
+//    public R<List<Dish>> list(Dish dish) {
+//
+//        //构造查询条件
+//        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+//        queryWrapper.eq(dish.getCategoryId() != null, Dish::getCategoryId, dish.getCategoryId());
+//
+//        //添加条件，查询状态为1（起售状态）的菜品
+//        queryWrapper.eq(Dish::getStatus, 1);
+//
+//        //添加排序条件
+//        queryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
+//
+//        List<Dish> list = dishService.list(queryWrapper);
+//
+//        return R.success(list);
+//    }
 
     /**
      * 新增菜品
@@ -139,6 +140,7 @@ public class DishController {
         return R.success(dishDto);
     }
 
+
     /**
      * 修改菜品
      * @param dishDto
@@ -152,6 +154,70 @@ public class DishController {
         dishService.updateWithFlavor(dishDto);
 
         return R.success("新增菜品成功");
+    }
+
+    /**
+     * 套餐管理中的方法  新增套餐里面根据菜品分类id（前端传过来的）查询菜品的方法 如上
+     * @param dish Dish来包装前端传过来的菜品分类id
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<DishDto>> list(Dish dish) {
+
+        List<DishDto> dishDtoList = null;
+
+        //构造查询条件
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(dish.getCategoryId() != null, Dish::getCategoryId, dish.getCategoryId());
+
+        //添加条件，查询状态为1（起售状态）的菜品
+        queryWrapper.eq(Dish::getStatus, 1);
+
+        //添加排序条件
+        queryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
+
+        // 这里应该是查询菜品信息，但是没有口味信息 没错
+        // 查询的是等于前端传过来菜品分类id的所有菜品信息，菜品表dish中查询的
+        List<Dish> list = dishService.list(queryWrapper);
+
+        dishDtoList = list.stream().map((item) -> {
+
+            DishDto dishDto = new DishDto();
+
+            BeanUtils.copyProperties(item, dishDto);
+
+            Long categoryId = item.getCategoryId(); // 菜品分类id
+            Category category = categoryService.getById(categoryId);  // 通过菜品分类id拿到分类
+            if(category != null) {
+                String categoryName = category.getName();  // 通过分类拿到 分类名称 ，估计是要显示的分类名称，DishDto里面有categoryName字段
+                dishDto.setCategoryName(categoryName);  // 就跟之前分页查询菜品时候要显示的分类名称
+            }
+            /**
+             * 通过菜品分类id查询分类下的菜品信息，遍历每一个菜品信息，
+             * 拿到菜品分类id，通过菜品分类id拿到分类信息，取出分类名称设置到DishDto
+             */
+
+            //当前菜品的id
+            Long dishId = item.getId();  // 拿到当前菜品的id，因为要查当前菜品的口味信息
+
+            LambdaQueryWrapper<DishFlavor> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+            lambdaQueryWrapper.eq(DishFlavor::getDishId, dishId);
+
+            //SQL: select * from dish_flavor where dish_id = ?
+            List<DishFlavor> dishFlavorList = dishFlavorService.list(lambdaQueryWrapper);
+
+            // 这里拿到了菜品口味信息！！！！移动端就能用上了
+            dishDto.setFlavors(dishFlavorList);
+            /**
+             * 通过菜品分类id查询分类下的菜品信息，遍历每一个菜品信息，
+             * 拿到菜品id，通过菜品id拿到菜品口味信息，设置DishDto中菜品口味信息
+             */
+
+            return dishDto;
+
+        }).collect(Collectors.toList());
+
+        return R.success(dishDtoList);
     }
 
 }
