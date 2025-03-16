60bebfca47d34df0c2da994e5b356caafc09a114
onesion <onesioner@163.com>
2025-03-16 15:15:54 +0800

    手机验证码登录-验证码登录

--- src/main/java/com/onesion/reggie/controller/UserController.java
+++ src/main/java/com/onesion/reggie/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.onesion.reggie.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.onesion.reggie.common.R;
 import com.onesion.reggie.entity.User;
 import com.onesion.reggie.service.UserService;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 /**
  * 移动端用户登录
@@ -53,6 +55,50 @@ public class UserController {
         }
 
         return R.error("短信改善失败");
+    }
+
+    /**
+     * 移动端用户登录
+     * @param map
+     * @param session
+     * @return
+     */
+    @PostMapping("/login")
+    public R<User> login(@RequestBody Map<String, String> map, HttpSession session) {
+
+        log.info(map.toString());
+
+        //获取手机号
+        String phone = map.get("phone").toString();
+        //获取验证码
+        String code = map.get("code").toString();
+
+
+        //从Session中获取保存的验证码
+        Object codeInSession = session.getAttribute(phone);
+
+
+        //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
+        if(codeInSession != null && codeInSession.equals(code)) {  //如果能够成功，说明登录成功
+
+            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(User::getPhone, phone);
+
+            User user = userService.getOne(queryWrapper);
+            if(user == null) {
+                //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+
+                userService.save(user);
+            }
+            session.setAttribute("user", user.getId());
+
+            return R.success(user);
+        }
+        return R.error("登录失败");
+
     }
 
 
