205ffc883be84e8b90b857e75f1576242cbb1e35
onesion <onesioner@163.com>
2025-03-16 17:39:45 +0800

    添加接口注解

--- src/main/java/com/onesion/reggie/controller/SetmealController.java
+++ src/main/java/com/onesion/reggie/controller/SetmealController.java
@@ -9,6 +9,10 @@ import com.onesion.reggie.entity.Setmeal;
 import com.onesion.reggie.service.CategoryService;
 import com.onesion.reggie.service.SetmealDishService;
 import com.onesion.reggie.service.SetmealService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +29,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/setmeal")
 @Slf4j
+@Api(tags = "套餐相关接口")
 public class SetmealController {
 
     @Autowired
@@ -43,6 +48,7 @@ public class SetmealController {
      */
     @PostMapping
     @CacheEvict(value = "setmealCache", allEntries = true)
+    @ApiOperation(value = "新增套餐接口")
     public R<String> save(@RequestBody SetmealDto setmealDto) {
 
         log.info("套餐信息：{}", setmealDto);
@@ -60,6 +66,12 @@ public class SetmealController {
      * @return
      */
     @GetMapping("/page")
+    @ApiOperation(value = "套餐分页查询接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page",value = "页码",required = true),
+            @ApiImplicitParam(name = "pageSize",value = "每页记录数",required = true),
+            @ApiImplicitParam(name = "name",value = "套餐名称",required = false)
+    })
     public R<Page> page(int page, int pageSize, String name) {
 
         //分页构造器对象
@@ -110,6 +122,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @ApiOperation(value = "套餐删除接口")
     @CacheEvict(value = "setmealCache", allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids) {
 
@@ -153,6 +166,7 @@ public class SetmealController {
      */
     @Cacheable(value = "setmealCache", key = "#setmeal.categoryId + '_' + #setmeal.status")  // 加入这个Cacheable注解。这个key跟当前查询条件有关系，可以是id和status
     @GetMapping("list")
+    @ApiOperation(value = "套餐条件查询接口")
     public R<List<Setmeal>> list(Setmeal setmeal) {
 
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
