2bbf724c5aeb15b678c30881ee7b327a0438a314
onesion <onesioner@163.com>
2025-03-16 17:05:31 +0800

    读写分离案例ShardingJDBC

---
+++ rw_demo/src/main/java/com/itheima/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.itheima.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.entity.User;
+import com.itheima.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import javax.sql.DataSource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private DataSource dataSource;
+    // 这里还注入了一个数据源对象，由于当前项目使用了shardingjdbc
+    // 这个时候这个数据源跟我们以往的数据源是不太一样的
+    // MasterSlaveDataSource
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User save(User user){
+        userService.save(user);
+        return user;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        userService.removeById(id);
+    }
+
+    @PutMapping
+    public User update(User user){
+        userService.updateById(user);
+        return user;
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id){
+        User user = userService.getById(id);
+        return user;
+    }
+
+    @GetMapping("/list")
+    public List<User> list(User user){
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(user.getId() != null,User::getId,user.getId());
+        queryWrapper.eq(user.getName() != null,User::getName,user.getName());
+        List<User> list = userService.list(queryWrapper);
+        return list;
+    }
+}
