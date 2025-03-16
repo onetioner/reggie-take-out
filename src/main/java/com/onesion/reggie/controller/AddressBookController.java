dd402cb1a03a8669f288c19974226155190354ba
onesion <onesioner@163.com>
2025-03-16 15:21:28 +0800

    新增地址

--- src/main/java/com/onesion/reggie/controller/AddressBookController.java
+++ src/main/java/com/onesion/reggie/controller/AddressBookController.java
@@ -1,8 +1,13 @@
 package com.onesion.reggie.controller;
 
+import com.onesion.reggie.common.BaseContext;
+import com.onesion.reggie.common.R;
+import com.onesion.reggie.entity.AddressBook;
 import com.onesion.reggie.service.AddressBookService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +23,18 @@ public class AddressBookController {
     private AddressBookService addressBookService;
 
 
+    /**
+     * 新增地址
+     */
+    @PostMapping
+    public R<AddressBook> save(@RequestBody AddressBook addressBook) {
+
+        addressBook.setUserId(BaseContext.getCurrentId());
+
+        log.info("addressBook:{}", addressBook);
+
+        addressBookService.save(addressBook);
+
+        return R.success(addressBook);
+    }
 }
