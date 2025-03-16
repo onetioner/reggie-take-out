5ed31958deed23a9186df863a1cdb13c74432869
onesion <onesioner@163.com>
2025-03-16 17:02:44 +0800

    清理缓存套餐数据

--- src/main/java/com/onesion/reggie/controller/SetmealController.java
+++ src/main/java/com/onesion/reggie/controller/SetmealController.java
@@ -12,6 +12,7 @@ import com.onesion.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,6 +42,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value = "setmealCache", allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto) {
 
         log.info("套餐信息：{}", setmealDto);
@@ -108,6 +110,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache", allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids) {
 
         log.info("ids:{}", ids);
