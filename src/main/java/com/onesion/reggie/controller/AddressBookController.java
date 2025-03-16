8d2379ea1ce7ed6ac8ce91537e9bfeb74d7e6021
onesion <onesioner@163.com>
2025-03-16 15:19:07 +0800

    导入地址薄相关代码

---
+++ src/main/java/com/onesion/reggie/controller/AddressBookController.java
@@ -0,0 +1,21 @@
+package com.onesion.reggie.controller;
+
+import com.onesion.reggie.service.AddressBookService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 地址簿管理
+ */
+@Slf4j
+@RestController
+@RequestMapping("/addressBook")
+public class AddressBookController {
+
+    @Autowired
+    private AddressBookService addressBookService;
+
+
+}
---
+++ src/main/java/com/onesion/reggie/service/AddressBookService.java
@@ -0,0 +1,7 @@
+package com.onesion.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.onesion.reggie.entity.AddressBook;
+
+public interface AddressBookService extends IService<AddressBook> {
+}
