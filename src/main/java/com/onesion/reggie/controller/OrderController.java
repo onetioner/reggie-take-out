8b288a8aa838a5f56e4723b0020e8a3b2c8db414
onesion <onesioner@163.com>
2025-03-16 15:47:38 +0800

    下单功能

--- src/main/java/com/onesion/reggie/controller/OrderController.java
+++ src/main/java/com/onesion/reggie/controller/OrderController.java
@@ -1,8 +1,12 @@
 package com.onesion.reggie.controller;
 
+import com.onesion.reggie.common.R;
+import com.onesion.reggie.entity.Orders;
 import com.onesion.reggie.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +20,20 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    /**
+     * 用户下单
+     * @param orders
+     * @return
+     */
+    @PostMapping("/submit")
+    public R<String> submit(@RequestBody Orders orders) {
+
+        log.info("订单数据：{}", orders);
+
+        orderService.submit(orders);
+
+        return R.success("下单成功");
+    }
     
 }
--- src/main/java/com/onesion/reggie/service/OrderService.java
+++ src/main/java/com/onesion/reggie/service/OrderService.java
@@ -4,5 +4,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.onesion.reggie.entity.Orders;
 
 public interface OrderService extends IService<Orders> {
-
+    /**
+     * 用户下单
+     * @param orders
+     */
+    public void submit(Orders orders);
 }
