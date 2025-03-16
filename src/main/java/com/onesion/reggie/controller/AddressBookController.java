2a3ed9d6bb106b32d733ca2f746ccc0b2f3b50d3
onesion <onesioner@163.com>
2025-03-16 15:23:26 +0800

    设置默认地址

--- src/main/java/com/onesion/reggie/controller/AddressBookController.java
+++ src/main/java/com/onesion/reggie/controller/AddressBookController.java
@@ -1,15 +1,13 @@
 package com.onesion.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.onesion.reggie.common.BaseContext;
 import com.onesion.reggie.common.R;
 import com.onesion.reggie.entity.AddressBook;
 import com.onesion.reggie.service.AddressBookService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 地址簿管理
@@ -34,6 +32,31 @@ public class AddressBookController {
         log.info("addressBook:{}", addressBook);
 
         addressBookService.save(addressBook);
+
+        return R.success(addressBook);
+    }
+
+    /**
+     * 设置默认地址
+     */
+    @PutMapping("default")
+    public R<AddressBook> setDefault(@RequestBody AddressBook addressBook) {
+
+        log.info("addressBook:{}", addressBook);
+
+        // 先将该用户所有地址的is_default更新为0
+        LambdaUpdateWrapper<AddressBook> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(AddressBook::getUserId, BaseContext.getCurrentId());
+        wrapper.set(AddressBook::getIsDefault, 0);
+
+        //SQL:update address_book set is_default = 0 where user_id = ?
+        addressBookService.update(wrapper);
+
+        // 然后将当前的设置的默认地址的is_default设置为1
+        addressBook.setIsDefault(1);
+
+        //SQL:update address_book set is_default = 1 where id = ?
+        addressBookService.updateById(addressBook);
 
         return R.success(addressBook);
     }
