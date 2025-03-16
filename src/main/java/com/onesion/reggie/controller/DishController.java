0432f92963e58ae22eccac4fa7bd64acba446f2b
onesion <onesioner@163.com>
2025-03-16 14:00:19 +0800

    菜品新增-菜品分类查询

---
+++ src/main/java/com/onesion/reggie/controller/DishController.java
@@ -0,0 +1,54 @@
+package com.onesion.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.onesion.reggie.common.R;
+import com.onesion.reggie.entity.Dish;
+import com.onesion.reggie.service.DishFlavorService;
+import com.onesion.reggie.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 菜品管理
+ */
+@Slf4j
+@RestController
+@RequestMapping("/dish")
+public class DishController {
+
+    @Autowired
+    private DishService dishService;
+
+    @Autowired
+    private DishFlavorService dishFlavorService;
+
+
+    /**
+     * 根据条件查询对应的菜品数据  就是根据菜品分类id，查询菜品
+     * @param dish
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<Dish>> list(Dish dish) {
+
+        //构造查询条件
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(dish.getCategoryId() != null, Dish::getCategoryId, dish.getCategoryId());
+
+        //添加条件，查询状态为1（起售状态）的菜品
+        queryWrapper.eq(Dish::getStatus, 1);
+
+        //添加排序条件
+        queryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
+
+        List<Dish> list = dishService.list(queryWrapper);
+
+        return R.success(list);
+    }
+
+}
---
+++ src/main/java/com/onesion/reggie/service/impl/DishServiceImpl.java
@@ -0,0 +1,16 @@
+package com.onesion.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.onesion.reggie.entity.Dish;
+import com.onesion.reggie.mapper.DishMapper;
+import com.onesion.reggie.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+
+
+
+}
