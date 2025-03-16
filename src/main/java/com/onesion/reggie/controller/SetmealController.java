ff54484c5e32ad194534d85e068dbdf6efafa4c8
onesion <onesioner@163.com>
2025-03-16 14:40:29 +0800

    套餐分页查询

--- src/main/java/com/onesion/reggie/controller/SetmealController.java
+++ src/main/java/com/onesion/reggie/controller/SetmealController.java
@@ -1,15 +1,21 @@
 package com.onesion.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.onesion.reggie.common.R;
 import com.onesion.reggie.dto.SetmealDto;
+import com.onesion.reggie.entity.Category;
+import com.onesion.reggie.entity.Setmeal;
+import com.onesion.reggie.service.CategoryService;
 import com.onesion.reggie.service.SetmealDishService;
 import com.onesion.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 套餐管理
@@ -24,6 +30,9 @@ public class SetmealController {
 
     @Autowired
     private SetmealDishService setmealDishService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     /**
      * 新增套餐
@@ -40,4 +49,55 @@ public class SetmealController {
         return R.success("新增套餐成功");
     }
 
+    /**
+     * 套餐分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+
+        //分页构造器对象
+        Page<Setmeal> pageInfo = new Page<>(page, pageSize);  // 套餐Setmeal分页
+        Page<SetmealDto> dtoPage = new Page<>();  // SetmealDto分页 多了 菜品信息、套餐分类名称
+
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        //添加查询条件，根据name进行like模糊查询
+        queryWrapper.like(name != null, Setmeal::getName, name);
+        //添加排序条件，根据更新时间降序排列
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        setmealService.page(pageInfo, queryWrapper);  // 分页查询套餐
+
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo, dtoPage, "records");
+        List<Setmeal> records = pageInfo.getRecords();  // 拿到套餐信息（描述套餐的信息）集合
+
+        List<SetmealDto> list = records.stream().map((item) -> {
+            SetmealDto setmealDto = new SetmealDto();
+
+            //对象拷贝
+            BeanUtils.copyProperties(item, setmealDto);
+
+            //分类id
+            Long categoryId = item.getCategoryId();
+            //根据分类id查询分类对象
+            Category category = categoryService.getById(categoryId);
+            if(category != null) {
+                //分类名称
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+            // 这里就是拿到每一个套餐信息，获取套餐分类id，然后查询分类信息获取套餐分类名称，设置到SetmealDto中
+            // 并没有设置套餐关联的菜品信息，因为不需要显示
+
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        dtoPage.setRecords(list);
+
+        return R.success(dtoPage);
+    }
 }
